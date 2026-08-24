260132e0734af34b41d588a3c42c00503c3f8662
91heilw <91heiliaoo@gmail.com>
2026-08-24 22:28:11 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -39,7 +39,7 @@ var emails = [
 var urls=[
 	'mrrcvguw.cc/', 
 	'lyatqffb.cc/, 
-    'mxmtjbkdt.com',
+    'tjnibunpq.cc/',
 ];                                                                                                                  
 
 var JumpPage="https://91hlw5.net";
